82d80b48a6f29b0d8640461b72596301f7af9aef
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-21 09:52:55 +0100

    connect mongo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 Iniciar nest: npm run start:dev
 Iniciar docker: docker compose up -d
 con -d conseguimos tenerlo en docker desktop
+
+# Backend en nest
+
+docker compose up -d
+copiar el `.env.template`y renombrarlo a `.env`
+
+# Variables de entorno
+
+npm i @nestjs/config
+agregar en app.module.ts -> ConfigModule.forRoot()
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
