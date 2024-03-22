d25cb9bf38e38522d35741c258f0045afe68040b
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-22 14:00:07 +0100

    finish

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(
     //! Pipe global para validar los datos de entrada, no se aceptan datos que no estén en el DTO
     new ValidationPipe({
